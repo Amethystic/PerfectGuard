774b29ed744d72fa1a00574cac0bdda826c0ceb6
soap <119145022+Amethystic@users.noreply.github.com>
2025-11-01 05:51:39 -0500

    Update - Intergrate Anti-Cheat Changed - "I just merged the code into one class thats all"

--- PerfectGuard/NetItemObjectManager.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Marioalexsan.PerfectGuard;
-
-public static class NetItemObjectManager
-{
-    public static readonly List<Net_ItemObject> AllItemObjects = new List<Net_ItemObject>();
-
-    public static int Count => AllItemObjects.Count;
-}
--- PerfectGuard/NetItemObjectTracker.cs
+++ PerfectGuard/NetItemObjectTracker.cs
@@ -2,6 +2,12 @@
 
 namespace Marioalexsan.PerfectGuard;
 
+public static class NetItemObjectManager
+{
+    public static readonly List<Net_ItemObject> AllItemObjects = new List<Net_ItemObject>();
+
+    public static int Count => AllItemObjects.Count;
+}
 
 public class NetItemObjectTracker : NetworkBehaviour
 {
